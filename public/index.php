fbfaa05c775f3f448ce8b32bdfebd2bfbb030477
dhreya07 <pagaldrea@gmail.com>
2025-11-25 10:12:06 +0800

    Hosted

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ while (ob_get_level()) {
 }
 
 // CORS headers
-header('Access-Control-Allow-Origin: http://localhost:5173');
+header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, X-User');
