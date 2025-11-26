8580334039dcc72a52a63e88cbb8278d308d0a0e
johnrhey <johnrheynedamotamares@gmail.com>
2025-11-26 08:22:14 +0800

    Update CORS origin for Google callback

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -9,7 +9,7 @@ public function googleCallback()
 {
     while (ob_get_level()) ob_end_clean();
 
-    header('Access-Control-Allow-Origin: http://localhost:5173');
+    header('Access-Control-Allow-Origin: https://ride-zones-front-end-liard.vercel.app');
     header('Access-Control-Allow-Credentials: true');
     header('Access-Control-Allow-Methods: POST, OPTIONS');
     header('Access-Control-Allow-Headers: Content-Type');
