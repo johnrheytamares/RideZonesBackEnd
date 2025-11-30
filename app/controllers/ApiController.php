bdf6b356baf54677e2cbf2eb9b2d2cad28e6c3ad
dhreya07 <pagaldrea@gmail.com>
2025-11-30 23:38:57 +0800

    Google

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -1323,7 +1323,7 @@ class ApiController extends Controller {
     public function googleCallback()
 {
     // CORS + Preflight
-    header('Access-Control-Allow-Origin: hhttp://localhost:5173');
+    header('Access-Control-Allow-Origin: http://localhost:5173');
     header('Access-Control-Allow-Methods: POST, OPTIONS');
     header('Access-Control-Allow-Headers: Content-Type');
     header('Content-Type: application/json');
