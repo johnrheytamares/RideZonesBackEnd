0a6c1db10d0c39ec2be4b281c23ef820656dec69
dhreya07 <pagaldrea@gmail.com>
2025-12-01 11:43:15 +0800

    CORS

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -1322,17 +1322,6 @@ class ApiController extends Controller {
 
     public function googleCallback()
 {
-    // CORS + Preflight
-    header('Access-Control-Allow-Origin: http://localhost:5173');
-    header('Access-Control-Allow-Methods: POST, OPTIONS');
-    header('Access-Control-Allow-Headers: Content-Type');
-    header('Content-Type: application/json');
-
-    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-        http_response_code(200);
-        exit();
-    }
-
     $input = json_decode(file_get_contents('php://input'), true);
     $token = $input['credential'] ?? null;
 
